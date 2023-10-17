46748bf7563cae8449a5409c2b5ce07aea8fa0fa
Masserini.20193 <Masserini.20193@E2-9-PC21>
2023-10-17 12:37:44 +0200

    Aggiungere i file di progetto.

---
+++ simulazione automobile/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simulazione_automobile
+{
+    internal class Class1
+    {
+        class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int speed { get; private set; }
+        }
+       
+    }
+}
