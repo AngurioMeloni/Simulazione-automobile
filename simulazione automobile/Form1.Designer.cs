c98134e797748b9e95a60bde679c0caf5ad61df9
Riccardo Masserini <masserini.riccardo.studente@itispaleocapa.it>
2023-10-18 21:51:00 +0200

    Implementazione cambio marcia

--- simulazione automobile/Form1.Designer.cs
+++ simulazione automobile/Form1.Designer.cs
@@ -39,11 +39,12 @@
             button9 = new Button();
             button10 = new Button();
             label1 = new Label();
+            textBox1 = new TextBox();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(302, 42);
+            button1.Location = new Point(302, 33);
             button1.Name = "button1";
             button1.Size = new Size(31, 29);
             button1.TabIndex = 0;
@@ -51,7 +52,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(339, 42);
+            button2.Location = new Point(339, 32);
             button2.Name = "button2";
             button2.Size = new Size(94, 29);
             button2.TabIndex = 1;
@@ -61,7 +62,7 @@
             // 
             // button3
             // 
-            button3.Location = new Point(460, 64);
+            button3.Location = new Point(448, 64);
             button3.Name = "button3";
             button3.Size = new Size(27, 29);
             button3.TabIndex = 2;
@@ -70,7 +71,7 @@
             // 
             // button4
             // 
-            button4.Location = new Point(460, 99);
+            button4.Location = new Point(448, 99);
             button4.Name = "button4";
             button4.Size = new Size(27, 29);
             button4.TabIndex = 3;
@@ -79,7 +80,7 @@
             // 
             // button5
             // 
-            button5.Location = new Point(493, 64);
+            button5.Location = new Point(481, 64);
             button5.Name = "button5";
             button5.Size = new Size(27, 29);
             button5.TabIndex = 4;
@@ -88,7 +89,7 @@
             // 
             // button6
             // 
-            button6.Location = new Point(493, 99);
+            button6.Location = new Point(481, 99);
             button6.Name = "button6";
             button6.Size = new Size(27, 29);
             button6.TabIndex = 5;
@@ -97,7 +98,7 @@
             // 
             // button7
             // 
-            button7.Location = new Point(526, 64);
+            button7.Location = new Point(514, 66);
             button7.Name = "button7";
             button7.Size = new Size(27, 29);
             button7.TabIndex = 6;
@@ -106,7 +107,7 @@
             // 
             // button8
             // 
-            button8.Location = new Point(526, 99);
+            button8.Location = new Point(514, 99);
             button8.Name = "button8";
             button8.Size = new Size(27, 29);
             button8.TabIndex = 7;
@@ -115,7 +116,7 @@
             // 
             // button9
             // 
-            button9.Location = new Point(526, 134);
+            button9.Location = new Point(514, 134);
             button9.Name = "button9";
             button9.Size = new Size(27, 29);
             button9.TabIndex = 8;
@@ -124,12 +125,13 @@
             // 
             // button10
             // 
-            button10.Location = new Point(152, 218);
+            button10.Location = new Point(565, 33);
             button10.Name = "button10";
-            button10.Size = new Size(94, 29);
+            button10.Size = new Size(123, 29);
             button10.TabIndex = 9;
-            button10.Text = "button10";
+            button10.Text = "Cambio Marcia";
             button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
             // 
             // label1
             // 
@@ -140,11 +142,19 @@
             label1.TabIndex = 10;
             label1.Text = "label1";
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(439, 33);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(120, 27);
+            textBox1.TabIndex = 11;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(914, 600);
+            Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(button10);
             Controls.Add(button9);
@@ -177,5 +187,6 @@
         private Button button9;
         private Button button10;
         private Label label1;
+        private TextBox textBox1;
     }
 }
--- simulazione automobile/Form1.cs
+++ simulazione automobile/Form1.cs
@@ -29,5 +29,86 @@ namespace simulazione_automobile
                 label1.Text = "Spenta";
             }
         }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(textBox1.Text) < 0 || int.Parse(textBox1.Text) > 6)
+            {
+                MessageBox.Show("Dato inserito non valido");
+            }
+            else if (car.ControlloMarcia(int.Parse(textBox1.Text)))
+            {
+                car.Marcia = int.Parse(textBox1.Text);
+                if (car.Marcia == 1)
+                {
+                    button6.BackColor = Color.White;
+                    button5.BackColor = Color.White;
+                    button4.BackColor = Color.White;
+                    button3.BackColor = Color.Red;
+                    button8.BackColor = Color.White;
+                    button7.BackColor = Color.White;
+                    button9.BackColor = Color.White;
+                }
+                if (car.Marcia == 2)
+                {
+                    button6.BackColor = Color.White;
+                    button5.BackColor = Color.White;
+                    button4.BackColor = Color.Red;
+                    button3.BackColor = Color.White;
+                    button8.BackColor = Color.White;
+                    button7.BackColor = Color.White;
+                    button9.BackColor = Color.White;
+                }
+                if (car.Marcia == 3)
+                {
+                    button6.BackColor = Color.White;
+                    button5.BackColor = Color.Red;
+                    button4.BackColor = Color.White;
+                    button3.BackColor = Color.White;
+                    button8.BackColor = Color.White;
+                    button7.BackColor = Color.White;
+                    button9.BackColor = Color.White;
+                }
+                if (car.Marcia == 4)
+                {
+                    button6.BackColor = Color.Red;
+                    button5.BackColor = Color.White;
+                    button4.BackColor = Color.White;
+                    button3.BackColor = Color.White;
+                    button8.BackColor = Color.White;
+                    button7.BackColor = Color.White;
+                    button9.BackColor = Color.White;
+                }
+                if (car.Marcia == 5)
+                {
+                    button6.BackColor = Color.White;
+                    button5.BackColor = Color.White;
+                    button4.BackColor = Color.White;
+                    button3.BackColor = Color.White;
+                    button8.BackColor = Color.White;
+                    button7.BackColor = Color.Red;
+                    button9.BackColor = Color.White;
+                }
+                if (car.Marcia == 6)
+                {
+                    button6.BackColor = Color.White;
+                    button5.BackColor = Color.White;
+                    button4.BackColor = Color.White;
+                    button3.BackColor = Color.White;
+                    button8.BackColor = Color.Red;
+                    button7.BackColor = Color.White;
+                }
+                if (car.Marcia == 0)
+                {
+                    button6.BackColor = Color.White;
+                    button5.BackColor = Color.White;
+                    button4.BackColor = Color.White;
+                    button3.BackColor = Color.White;
+                    button8.BackColor = Color.White;
+                    button7.BackColor = Color.White;
+                    button9.BackColor = Color.Red;
+                }
+            }
+        }
     }
 }
