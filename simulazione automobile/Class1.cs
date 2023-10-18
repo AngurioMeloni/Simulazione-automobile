0b7113a3107dc747b778c3aedd9289beea0483c8
Riccardo Masserini <masserini.riccardo.studente@itispaleocapa.it>
2023-10-18 21:06:15 +0200

    costruttore attributi e get e set

--- simulazione automobile/Class1.cs
+++ simulazione automobile/Class1.cs
@@ -6,14 +6,60 @@ using System.Threading.Tasks;
 
 namespace simulazione_automobile
 {
-    internal class Class1
+    internal class Car
     {
-        class Car
+        //da 0 a 100, 6 marce
+        protected int _vel;
+
+        //6 marce diverse
+        protected int _marcia;
+
+        //booleano accensione
+        protected bool _acc;
+
+        //Attributo che servirà a calcolare aìl'accelerazione
+        protected int _limit;
+
+        //attributo che serve a calcolare la decellerazione 
+        protected int _total;
+
+        public Car()
         {
-            public string Make { get; set; }
-            public string Model { get; set; }
-            public int speed { get; private set; }
+            Vel = 0;
+            Marcia = 0;
+            Acc = false;
+            Limit = 0;
         }
-       
+        #region get e set
+        public int Vel
+        {
+            get { return _vel; }
+            set { _vel = value; }
+        }
+
+        public int Marcia
+        {
+            get { return _marcia; }
+            set { _marcia = value; }
+        }
+
+        public bool Acc
+        {
+            get { return _acc; }
+            set { _acc = value; }
+        }
+
+        public int Limit
+        {
+            get { return _limit; }
+            set { _limit = value; }
+        }
+
+        public int Total
+        {
+            get { return _total; }
+            set { _total = value; }
+        }
+        #endregion
     }
 }
